e46aeca535ad68c4ce8fd776964ffcc4b4977a8e
PraveenaP <praveena.chowdhary28@gmail.com>
2024-09-04 10:39:07 +0530

    tee

---
+++ mysql.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+LOGS_FOLDER="/var/log/expense"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+
+
+mkdir -p $LOGS_FOLDER
+
+USERID=$(id -u)
+echo "user id is: $USERID" &>>$LOG_FILE 
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is $R failed $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 is $G success $N" | tee -a $LOG_FILE
+    fi
+}
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R please run the script with root priveleges $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+echo "script started executing at: $(date)" | tee -a $LOG_FILE
+CHECK_ROOT
+
+dnf install mysql-server -y
+VALIDATE $? "Installing MySQL Server"
+
+systemctl enable mysqld
+VALIDATE $? "Enabled MySQL Server"
+
+systemctl start mysqld
+VALIDATE $? "Started MySQL Server"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1
+VALIDATE $? "Setting up root password"
