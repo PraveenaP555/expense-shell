a9456f34e43062295761ce4b2c13caa77a8b9bdc
PraveenaP <praveena.chowdhary28@gmail.com>
2024-09-04 14:24:24 +0530

    tee

---
+++ backend.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+LOGS_FOLDER="/var/log/expense"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+
+
+mkdir -p $LOGS_FOLDER
+
+USERID=$(id -u)
+echo "user id is: $USERID" &>>$LOG_FILE 
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is $R failed $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 is $G success $N" | tee -a $LOG_FILE
+    fi
+}
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R please run the script with root priveleges $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+echo "script started executing at: $(date)" | tee -a $LOG_FILE
+CHECK_ROOT
+
+dnf module disable nodejs -y
+VALIDATE $? "Disable default node js"
+
+dnf module enable nodejs:20 -y
+VALIDATE $? "Enable node js 20"
+
+dnf install nodejs -y
+VALIDATE $? "Install node js 20"
+
+useradd expense
+VALIDATE $? "Creating expense user"
