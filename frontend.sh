691a884ab93daf20a1f48792a86dfa8c50981362
PraveenaP <praveena.chowdhary28@gmail.com>
2024-09-05 10:47:49 +0530

    praveena

--- frontend.sh
+++ frontend.sh
@@ -55,5 +55,7 @@ cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>$LOG_FILE
 VALIDATE $? "Extracting frontend code"
 
+cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf
+
 systemctl restart nginx
 VALIDATE $? "frontend started"
